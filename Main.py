6941a3a696dbad5842ca27d105ead9fbb3d44ddc
Marco Calamo (IlKaiser) <samuele.rinaldi90@gmail.com>
2020-12-10 23:03:51 +0100

    Refined Main

--- Main.py
+++ Main.py
@@ -1,14 +1,31 @@
 from Alice import Alice
 from Bob import Bob
+from Cert import Cert
 import threading
+import time
 
 a = Alice()
 b = Bob()
+c = Cert()
 
+time.sleep(0.1)
 
-b.start()
+c.start()
 a.start()
+time.sleep(0.1)
+b.start()
+time.sleep(0.1)
 
-a.connect_to_bob()
+
+
+### Initial seutp
+"""
+    A ----> C
+    C <---> B
+"""
+b.connect_to_cert()
+time.sleep(0.5)
 a.connect_to_cert()
+
+
 
